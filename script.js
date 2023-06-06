22939a06cdf5775372d8fa1399873c21336f38f3
tsohn1 <sohnte@gmail.com>
2023-06-07 02:54:31 +0900

    Add "Clear" and "Enter Dimensions" buttons

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
+let rows = 16;
+let cols = 16;
+
 function fill_square() {
   this.classList.add("filled");
 }
 
-function unfill_square() {
-  this.classList.remove("filled");
+function clear_squares() {
+  const square = document.querySelectorAll(".square");
+  square.forEach(element => { element.classList.remove("filled");
+    
+  });
 }
 
-let rows = 16;
-let cols = 16;
+function make_grid() {
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement('div');
+    row.classList.add("row");
+    grid.appendChild(row);
+    for (let j = 0; j < cols; j++) {
+      const square = document.createElement('div');
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+  }
+  const square = document.querySelectorAll(".square");
+  square.forEach(element => {
+    element.addEventListener("mouseenter", fill_square);
+  });
+  clear.addEventListener("click", clear_squares);
+  dimensions.addEventListener("click", prompt_dimensions);
+}
 
+function prompt_dimensions() {
+  let user_input = prompt("please enter a number between 1~100:");
+  let new_dimensions = parseInt(user_input);
+
+  while (new_dimensions < 1 || new_dimensions > 100 || isNaN(new_dimensions)) {
+    user_input = prompt("your number is out of range. Please enter a number between 1~100:");
+    new_dimensions = parseInt(user_input);
+  }
+
+  rows = new_dimensions;
+  cols = new_dimensions;
+  while(grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  make_grid();
+}
 
 const grid = document.querySelector('.grid');
-for (let i = 0; i < rows; i++) {
-  const row = document.createElement('div');
-  row.classList.add("row");
-  grid.appendChild(row);
-  for (let j = 0; j < cols; j++) {
-    const square = document.createElement('div');
-    square.classList.add("square");
-    row.appendChild(square);
-  }
-}
-
-
-const square = document.querySelectorAll(".square");
-square.forEach(element => {
-  element.addEventListener("mouseenter", fill_square);
-});
-
-square.forEach(element => {
-  element.addEventListener("mouseleave", unfill_square);
-});
+const clear = document.querySelector(".clear");
+const dimensions = document.querySelector(".dimensions");
+make_grid();
